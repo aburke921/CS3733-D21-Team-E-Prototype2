fe7bc635bb469673913dc1f7830ec48166631976
RVRX <colemanning@yandex.com>
2021-04-04 18:18:21 -0400

    Update package name to "TeamE," and fixed all references to it. closes #26
    only concern here is simplelogger.properties, which now apparently has an "unused property" - it appears to only be a gradle log file of some sort so I believe it should be okay

--- src/main/java/edu/wpi/TeamE/Main.java
+++ src/main/java/edu/wpi/TeamE/Main.java
@@ -1,4 +1,4 @@
-package edu.wpi.teamname;
+package edu.wpi.TeamE;
 
 public class Main {
 
--- src/main/java/edu/wpi/TeamE/views/Scene1.java
+++ src/main/java/edu/wpi/TeamE/views/Scene1.java
@@ -1,6 +1,6 @@
-package edu.wpi.teamname.views;
+package edu.wpi.TeamE.views;
 
-import edu.wpi.teamname.App;
+import edu.wpi.TeamE.App;
 import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -12,7 +12,7 @@ public class Scene1 {
   @FXML
   private void advanceScene(ActionEvent e) {
     try {
-      Parent root = FXMLLoader.load(getClass().getResource("/edu/wpi/teamname/fxml/Scene2.fxml"));
+      Parent root = FXMLLoader.load(getClass().getResource("/edu/wpi/TeamE/fxml/Scene2.fxml"));
       App.getPrimaryStage().getScene().setRoot(root);
     } catch (IOException ex) {
       ex.printStackTrace();

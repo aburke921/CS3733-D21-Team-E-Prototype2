e50d0f62fffccc5c2a4e039e0aec409206e8005f
Seamus Sullivan <sheadog131313@gmail.com>
2021-04-06 19:46:07 -0400

    Security Service Requests Added methods to security service requests page to allow for Addresses #17

--- src/main/java/edu/wpi/TeamE/views/SecurityService.java
+++ src/main/java/edu/wpi/TeamE/views/SecurityService.java
@@ -47,4 +47,30 @@ public class SecurityService {
         }
     }
 
+    @FXML
+    public void selectSecurityLevel(ActionEvent action) {
+        String level =((MenuItem) action.getSource()).getText();
+        levelOfSecurity.setText(level);
+    }
+
+    @FXML
+    public void selectUrgency(ActionEvent action) {
+        String urgency =((MenuItem) action.getSource()).getText();
+        levelOfUrgency.setText(urgency);
+    }
+
+
+
+    @FXML
+    void initialize() {
+        assert helpSecurityService != null : "fx:id=\"helpSecurityService\" was not injected";
+        assert locationOfDelivery != null : "fx:id=\"locationOfDelivery\" was not injected";
+        assert levelOfSecurity != null : "fx:id=\"levelOfSecurity\" was not injected";
+        assert levelOfUrgency != null : "fx:id=\"levelOfUrgency\" was not injected";
+        assert reasonForRequest != null : "fx:id=\"reasonForRequest\" was not injected";
+        assert submitSecurityRequest != null : "fx:id=\"submitSecurityRequest\" was not injected";
+        assert cancelSecurityRequest != null : "fx:id=\"cancelSecurityRequest\" was not injected";
+
+    }
+
 }

c82282c5a939a1efbb379ae67c98033a77788dbd
aburke921 <aburke@wpi.edu>
2021-04-10 15:14:14 -0400

    Added makeConnection class and makeConnection() constructor This way it can be easy to pass around the connection that is needed to perform many of the querying functions

--- src/main/java/edu/wpi/TeamE/databases/makeConnection.java
+++ src/main/java/edu/wpi/TeamE/databases/makeConnection.java
@@ -5,38 +5,33 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
-public class functions {
+public class makeConnection {
 
+    Connection connection;
 
-    public static Connection makeConnection() {
-
+    //Constructor
+    public makeConnection(){
         // Initialize DB
         System.out.println("Starting connection to Apache Derby\n");
-        Connection connection;
         try {
 
+            //Makes it so a username and password (hardcoded) is needed to access the database data
             Properties props = new Properties();
             props.put("user", "admin");
             props.put("password", "admin");
 
             Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
 
-            // remove :memory to have persistent database
             try {
                 connection = DriverManager.getConnection("jdbc:derby:/Users/ashley/Documents/IntelliJ_Projects/Algo2/Database;create=true", props);
-
-//                refactorTables(connection);
-
-                // deleteAllTables(connection);
-
-                return connection;
-
             } catch (SQLException e) {
                 // e.printStackTrace();
             }
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
-        return null;
+
     }
+
+
 }

03e8d104bfc795df4995542b2215361084271793
Cole Manning // RVRX <colemanning@yandex.com>
2021-04-12 16:24:56 -0400

    Added resolve ID from longName

--- src/main/java/edu/wpi/TeamE/views/PathFinder.java
+++ src/main/java/edu/wpi/TeamE/views/PathFinder.java
@@ -55,6 +55,14 @@ public class PathFinder {
     @FXML // fx:id="pane"
     private Pane pane;
 
+    private String startNodeID; // selected starting value's ID
+
+    private String endNodeID; // selected ending value's ID
+
+    ObservableList<String> list = FXCollections.observableArrayList(); //declaration
+
+    ObservableList<String> listOfId = FXCollections.observableArrayList(); //declaration
+
     /**
      * Returns to {@link edu.wpi.TeamE.views.Default} page.
      * @param event calling event info
@@ -76,68 +84,72 @@ public class PathFinder {
 
     /**
      * Gets selected item from {@link #startLocationList} dropdown.
-     * todo, chosen result should be saved to global variable?
-     * todo, judging from comments, this will get full node info from DB?
+     * todo, judging from comments, this will get full node info from DB? For now, I am using my own resolveID
      * @param event calling event info
      */
     @FXML
     void selectStartNode(ActionEvent event) {
-        String nodeID = ((JFXComboBox) event.getSource()).getValue().toString();
-        System.out.println("selected Start nodeID: " + nodeID);
+        String dropdownSelected = ((JFXComboBox) event.getSource()).getValue().toString();
+        System.out.println("selected Start dropdownSelected (node Name): " + dropdownSelected);
+        startNodeID = resolveID(dropdownSelected);
+        System.out.println("node ID resolved to: " + startNodeID);
 
-        /*
-        String longName = getLongName(nodeID); //Given by database
-        startLocationList.setValue(longName);
-        */
+//        String longName = getLongName(dropdownSelected); //Given by database
+//        startLocationList.setValue(longName);
 
     }
 
     /**
      * Gets selected item from {@link #endLocationList} dropdown.
-     * todo, chosen result should be saved to global variable?
-     * todo, judging from comments, this will get full node info from DB?
+     * todo, judging from comments, this will get full node info from DB? For now, I am using my own resolveID
      * @param event calling event info
      */
     @FXML
     void selectEndNode(ActionEvent event) {
-        String nodeID = ((JFXComboBox) event.getSource()).getValue().toString();
-        System.out.println("selected End nodeID: " + nodeID);
-        /*
-        String longName = getLongName(nodeID); //Given by database
-        endLocationList.setValue(longName);
-        */
+        String dropdownSelected = ((JFXComboBox) event.getSource()).getValue().toString();
+        System.out.println("selected End dropdownSelected: " + dropdownSelected);
+        endNodeID = resolveID(dropdownSelected);
+        System.out.println("node ID resolved to: " + startNodeID);
+
+//        String longName = getLongName(dropdownSelected); //Given by database
+//        endLocationList.setValue(longName);
     }
 
+    /**
+     * todo
+     * @param event calling fcn's (Find Path Button) event info
+     */
     @FXML
     public void findPath(ActionEvent event) {
 
-        double xcoord = (double) 1748 / 10;
-        double ycoord = (double) 1336 / 10;
+        //todo commented section here serves as good example for how shapes will be made
+//        double xcoord = (double) 1748 / 10;
+//        double ycoord = (double) 1336 / 10;
+//
+//        double xcoord2 = (double) 1910 / 10;
+//        double ycoord2 = (double) 1465 / 10;
+//
+//        Circle circle = new Circle(xcoord, ycoord, 2, Color.RED);
+//        Circle circle2 = new Circle(xcoord2, ycoord2, 2, Color.RED);
+//        Line line = new Line(xcoord, ycoord, xcoord2, ycoord2);
+//        line.setStroke(Color.RED);
+//
+//        pane.getChildren().addAll(circle, circle2, line);
 
-        double xcoord2 = (double) 1910 / 10;
-        double ycoord2 = (double) 1465 / 10;
 
-        Circle circle = new Circle(xcoord, ycoord, 2, Color.RED);
-        Circle circle2 = new Circle(xcoord2, ycoord2, 2, Color.RED);
-        Line line = new Line(xcoord, ycoord, xcoord2, ycoord2);
-        line.setStroke(Color.RED);
-
-        pane.getChildren().addAll(circle, circle2, line);
-
-
-
-        //todo potential start of findPath()
-        System.out.println("FINDING PATH...");
+        System.out.print("\nFINDING PATH...");
 
         //get selected nodes
-        String startNode = ""; //todo, need to resolve ID from dropdown values
-        String endNode = ""; //todo, need to resolve ID from dropdown values
+        String startNode = startNodeID;
+        String endNode = endNodeID;
+        assert startNodeID != null && endNodeID != null; //todo, actual button validation
 
         //Execute A* Search
+        System.out.print("A* Search with startNodeID of " + startNode + ", and endNodeID of " + endNodeID + "\n");
         Searcher aStar = new AStarSearch();
-        Path foundPath = aStar.search(startNode, endNode);
+        Path foundPath = aStar.search(startNode, endNode); //todo error here - this is a question for Algo.
 
-        //build path
+        //build path todo
         Iterator<edu.wpi.TeamE.algorithms.pathfinding.Node> nodeIterator = foundPath.iterator(); //create iterable list
         while(nodeIterator.hasNext()){ //loop through list
             //this iterator will return a Node object
@@ -161,7 +173,7 @@ public class PathFinder {
 
     /**
      * Method called by FXMLLoader when initialization is complete. Propagates initial fields in FXML.
-     * Namely, adds FloorMap PNG, todo fills dropdowns with DB data
+     * Namely, adds FloorMap PNG, fills dropdowns with DB data
      */
     @FXML // This method is called by the FXMLLoader when initialization is complete
     void initialize() {
@@ -172,15 +184,13 @@ public class PathFinder {
         imageView.setImage(image);
 
 
-        //DB connection todo doesnt always work after Map Editor has muddled the DB
+        //DB connection todo doesnt always work after Map Editor has muddled the DB?
         makeConnection connection = new makeConnection();
 
         //get All nodes
         ArrayList<Node> nodeArrayList = connection.getAllNodes();
 
-        //convert/add to Observable List
-        ObservableList<String> list = FXCollections.observableArrayList();
-        ObservableList<String> listOfId = FXCollections.observableArrayList(); //todo
+        //add to Observable List
         for (Node node : nodeArrayList) { //loop through list
             //this iterator will return a Node object
             //which is just a container for all the node info like id, floor, building, etc
@@ -199,11 +209,32 @@ public class PathFinder {
 
             //add to list
             list.add(longName);
-            listOfId.add(id); //todo
+            listOfId.add(id); //for ID lookups later todo maybe just use nodeArrayList?
         }
 
         //add ObservableList to dropdowns
         startLocationList.setItems(list);
         endLocationList.setItems(list);
+    }
+
+    /**
+     * Gets a node's ID from it's longName
+     *
+     * Implementation: Searches through "list" until longName is found,
+     *  Then gets "listOfId"'s value at this index. Both lists must be initialised
+     *  for proper functioning
+     *
+     * @param longName "longName" of node to be resolved
+     * @return Node ID
+     */
+    private String resolveID(String longName) {
+        assert list.size() == listOfId.size();
+        for (int i = 0; i < list.size(); i++) {
+            if (longName.equals(list.get(i))) {
+                System.out.println("Correlating index is " + i);
+                return listOfId.get(i); //found correlating index
+            }
+        }
+        return null;
     }
 }

b1f546e45ef45b5b22d5434dc2900cf38e551ba6
aburke921 <aburke@wpi.edu>
2021-04-13 16:19:37 -0400

    JUnit test case for getListofNodeIDS()

--- src/test/java/edu/wpi/TeamE/DatabaseTests.java
+++ src/test/java/edu/wpi/TeamE/DatabaseTests.java
@@ -1,13 +1,46 @@
 package edu.wpi.TeamE;
 
+import edu.wpi.TeamE.databases.makeConnection;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.File;
+import java.util.ArrayList;
+
 
 public class DatabaseTests {
 
-//	make new connection
-//
-//	edit the db
-//
-//	@Test
-//	assertEqual xxx, xxx
+    @Test
+    @DisplayName("testGetListofNodeIDS")
+    public void testGetListofNodeIDS(){
+
+        makeConnection connection = makeConnection.makeConnection();
+
+        File nodes = new File("src/main/resources/edu/wpi/TeamE/csv/bwEnodes.csv");
+        File edges = new File("src/main/resources/edu/wpi/TeamE/csv/bwEedges.csv");
+
+        try {
+            connection.deleteAllTables();
+            connection.createTables();
+            System.out.println("Tables were reset");
+        } catch (Exception e) {
+            connection.createTables();
+            System.out.println("Tables were created");
+        }
+
+        connection.addNode("test1", 0, 0,"test", "test", "test", "test", "test");
+        connection.addNode("test2", 2, 2,"test", "test", "test", "test", "test");
+        connection.addNode("test3", 3, 3,"test", "test", "test", "test", "test");
+        connection.addNode("test4", 4, 4,"test", "test", "test", "test", "test");
+
+        ArrayList<String> listOfNodeIDs = new ArrayList<>();
+
+        listOfNodeIDs.add("test1");
+        listOfNodeIDs.add("test2");
+        listOfNodeIDs.add("test3");
+        listOfNodeIDs.add("test4");
+
+        assertTrue(listOfNodeIDs.equals(connection.getListofNodeIDS()));
+    }
 }

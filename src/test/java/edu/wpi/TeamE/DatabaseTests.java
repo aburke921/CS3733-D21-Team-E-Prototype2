65d9bba5a8e564f0544139aa6f0d714ffc2757c3
GP2P <73323107+GP2P@users.noreply.github.com>
2021-04-14 20:28:57 -0400

    Optimized DB init time, stopped deleting all tables +1

--- src/test/java/edu/wpi/TeamE/DatabaseTests.java
+++ src/test/java/edu/wpi/TeamE/DatabaseTests.java
@@ -11,6 +11,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.lang.reflect.Array;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import edu.wpi.TeamE.algorithms.pathfinding.Node;
 
@@ -34,7 +35,11 @@ public class DatabaseTests {
             connection.createTables();
             System.out.println("Tables were reset");
         } catch (Exception e) {
-            connection.createTables();
+            try {
+                connection.createTables();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
             System.out.println("Tables were created");
         }
     }

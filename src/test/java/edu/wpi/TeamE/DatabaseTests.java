025103f517b43d8bd86b16f527f42fc1c0ba2b7b
aburke921 <aburke@wpi.edu>
2021-04-15 15:40:08 -0400

    added more JUnit test cases - testGetAllNodesByFloor: 2 nodes with the given floor - testGetAllNodesByFloor: no nodes with the given floor - testGetAllNodeLongNamesByFloor: 2 nodes with the given floor - testGetAllNodeLongNamesByFloor: no nodes with the given floor - testGetListOfNodeIDSByFloor: 2 nodes with the given floor - testGetListOfNodeIDSByFloor: no nodes with the given floor - testGetAllEdges: there are no edges - testAddNode: the node added already exists - testAddEdge: the startNode does not exist - testAddEdge: the endNode does not exist

--- src/test/java/edu/wpi/TeamE/DatabaseTests.java
+++ src/test/java/edu/wpi/TeamE/DatabaseTests.java
@@ -1,6 +1,8 @@
 package edu.wpi.TeamE;
 
 import edu.wpi.TeamE.databases.makeConnection;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.util.Pair;
 import org.junit.jupiter.api.*;
 
@@ -11,6 +13,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.lang.reflect.Array;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import edu.wpi.TeamE.algorithms.pathfinding.Node;
 
@@ -34,8 +37,12 @@ public class DatabaseTests {
             connection.createTables();
             System.out.println("Tables were reset");
         } catch (Exception e) {
-            connection.createTables();
-            System.out.println("Tables were created");
+            try {
+                connection.createTables();
+                System.out.println("Tables were created");
+            }catch(SQLException a){
+                a.printStackTrace();
+            }
         }
     }
 
@@ -50,6 +57,20 @@ public class DatabaseTests {
 
         assertTrue(testNode1.equals(testNode2));
     }
+
+
+    @Test
+    @DisplayName("GetNodeInfo for a node with null values")
+    public void testGetNodeInfo2(){
+        connection.addNode("test123", 12, 13, "floor", null, null, null, null);
+
+        Node testNode1 = new Node("test123", 12, 13, "floor", null, null, null, null);
+
+        Node testNode2 = connection.getNodeInfo("test123");
+
+        assertTrue(testNode1.equals(testNode2));
+    }
+
 
     @Test
     @DisplayName("testGetNodeLite")
@@ -175,6 +196,146 @@ public class DatabaseTests {
 
 
     @Test
+    @DisplayName("testGetAllNodesByFloor: 2 nodes with the given floor")
+    public void testGetAllNodesByFloor(){
+        connection.addNode("nodeID1", 0, 0, "1", "building1", "nodeType1", "longName1", "shortName1");
+        connection.addNode("nodeID2", 1, 0, "1", "building2", "nodeType2", "longName2", "shortName2");
+        connection.addNode("nodeID3", 0, 0, "2", "building3", "nodeType3", "longName3", "shortName3");
+
+        ArrayList<Node> nodes = connection.getAllNodesByFloor("1");
+
+        ArrayList<Node> correctNodes = new ArrayList<Node>();
+        Node node1 = new Node("nodeID1", 0, 0, "1", "building1", "nodeType1", "longName1", "shortName1");
+        Node node2 = new Node("nodeID2", 1, 0, "1", "building2", "nodeType2", "longName2", "shortName2");
+
+        correctNodes.add(node1);
+        correctNodes.add(node2);
+
+        boolean allCorrect = true;
+        boolean nodeID = false;
+        boolean xCoord = false;
+        boolean yCoord = false;
+        boolean floor = false;
+        boolean building = false;
+        boolean nodeType = false;
+        boolean longName = false;
+        boolean shortName = false;
+
+        if(nodes.size() == correctNodes.size()){
+            for(int node = 0; node < nodes.size(); node++){
+                Node returnedNode = nodes.get(node);
+                Node correctNode = correctNodes.get(node);
+                if(returnedNode.get("id").equals(correctNode.get("id"))){
+                    nodeID = true;
+                }
+                if(returnedNode.getX() == correctNode.getX()){
+                    xCoord = true;
+                }
+                if(returnedNode.getY() == correctNode.getY()){
+                    yCoord = true;
+                }
+                if(returnedNode.get("floor").equals(correctNode.get("floor"))){
+                    floor = true;
+                }
+                if(returnedNode.get("building").equals(correctNode.get("building"))){
+                    building = true;
+                }
+                if(returnedNode.get("type").equals(correctNode.get("type"))){
+                    nodeType = true;
+                }
+                if(returnedNode.get("longName").equals(correctNode.get("longName"))){
+                    longName = true;
+                }
+                if(returnedNode.get("shortName").equals(correctNode.get("shortName"))){
+                    shortName = true;
+                }
+                if(nodeID && xCoord && yCoord && floor && building && nodeType && longName && shortName == false){
+                    allCorrect = false;
+                }
+            }
+        }
+        else{
+            allCorrect = false;
+        }
+
+        assertTrue(allCorrect);
+    }
+
+    @Test
+    @DisplayName("testGetAllNodesByFloor: no nodes with the given floor")
+    public void testGetAllNodesByFloor2(){
+        connection.addNode("nodeID1", 0, 0, "1", "building1", "nodeType1", "longName1", "shortName1");
+        connection.addNode("nodeID2", 1, 0, "1", "building2", "nodeType2", "longName2", "shortName2");
+        connection.addNode("nodeID3", 0, 0, "2", "building3", "nodeType3", "longName3", "shortName3");
+
+        ArrayList<Node> nodes = connection.getAllNodesByFloor("3");
+
+        assertTrue(nodes.size() == 0);
+    }
+
+
+    @Test
+    @DisplayName("testGetAllNodeLongNamesByFloor: 2 nodes with the given floor")
+    public void testGetAllNodeLongNamesByFloor(){
+        connection.addNode("nodeID1", 0, 0, "1", "building1", "nodeType1", "longName1", "shortName1");
+        connection.addNode("nodeID2", 1, 0, "1", "building2", "nodeType2", "longName2", "shortName2");
+        connection.addNode("nodeID3", 0, 0, "2", "building3", "nodeType3", "longName3", "shortName3");
+
+        ObservableList<String> longNames = connection.getAllNodeLongNamesByFloor("1");
+
+        ObservableList<String> correctLongNames =  FXCollections.observableArrayList();
+
+        correctLongNames.add("longName1");
+        correctLongNames.add("longName2");
+
+        assertTrue(longNames.equals(correctLongNames));
+
+    }
+
+    @Test
+    @DisplayName("testGetAllNodeLongNamesByFloor: no nodes with the given floor")
+    public void testGetAllNodeLongNamesByFloor2(){
+        connection.addNode("nodeID1", 0, 0, "1", "building1", "nodeType1", "longName1", "shortName1");
+        connection.addNode("nodeID2", 1, 0, "1", "building2", "nodeType2", "longName2", "shortName2");
+        connection.addNode("nodeID3", 0, 0, "2", "building3", "nodeType3", "longName3", "shortName3");
+
+        ObservableList<String> longNames = connection.getAllNodeLongNamesByFloor("3");
+
+        assertTrue(longNames.size() == 0);
+    }
+
+    @Test
+    @DisplayName("testGetListOfNodeIDSByFloor: 2 nodes with the given floor")
+    public void testGetListOfNodeIDSByFloor(){
+        connection.addNode("nodeID1", 0, 0, "1", "building1", "nodeType1", "longName1", "shortName1");
+        connection.addNode("nodeID2", 1, 0, "1", "building2", "nodeType2", "longName2", "shortName2");
+        connection.addNode("nodeID3", 0, 0, "2", "building3", "nodeType3", "longName3", "shortName3");
+
+        ArrayList<String> nodeIDs = connection.getListOfNodeIDSByFloor("1");
+
+        ArrayList<String> correctNodeIDs =  new ArrayList<String>();
+
+        correctNodeIDs.add("nodeID1");
+        correctNodeIDs.add("nodeID2");
+
+        assertTrue(nodeIDs.equals(correctNodeIDs));
+    }
+
+    @Test
+    @DisplayName("testGetListOfNodeIDSByFloor: no nodes with the given floor")
+    public void testGetListOfNodeIDSByFloor2(){
+        connection.addNode("nodeID1", 0, 0, "1", "building1", "nodeType1", "longName1", "shortName1");
+        connection.addNode("nodeID2", 1, 0, "1", "building2", "nodeType2", "longName2", "shortName2");
+        connection.addNode("nodeID3", 0, 0, "2", "building3", "nodeType3", "longName3", "shortName3");
+
+        ArrayList<String> nodeIDs = connection.getListOfNodeIDSByFloor("3");
+
+
+        assertTrue(nodeIDs.size() == 0);
+    }
+
+
+    @Test
     @DisplayName("testGetAllEdges")
     public void testGetAllEdges(){
         connection.addNode("test1", 0, 0,"test", "test", "test", "test", "test");
@@ -219,6 +380,18 @@ public class DatabaseTests {
         assertTrue(allCorrect);
     }
 
+
+    @Test
+    @DisplayName("testGetAllEdges: there are no edges")
+    public void testGetAllEdges2(){
+        connection.addNode("test1", 0, 0,"test", "test", "test", "test", "test");
+        connection.addNode("test2", 2, 2,"test", "test", "test", "test", "test");
+
+        ArrayList<Edge> listOfEdges = connection.getAllEdges();
+
+        assertTrue(listOfEdges.size() == 0);
+    }
+
     @Test
     @DisplayName("testAddNode")
     public void testAddNode(){
@@ -230,6 +403,18 @@ public class DatabaseTests {
 
         assertTrue(testResult == 1);
     }
+
+    @Test
+    @DisplayName("testAddNode: the node added already exists")
+    public void testAddNode2(){
+        // if this works, testResult should be 1
+        connection.addNode("testNode", 111, 222, "h1", "String", "String", "String", "String");
+
+        int testResult = connection.addNode("testNode", 111, 222, "h1", "String", "String", "String", "String");
+
+        assertTrue(testResult == 0);
+    }
+
 
     @Test
     @DisplayName("testAddEdge")
@@ -246,6 +431,42 @@ public class DatabaseTests {
 
         assertTrue(testResult == 1);
 
+    }
+
+    @Test
+    @DisplayName("testAddEdge: the edgeID allready exists")
+    public void testAddEdge2(){
+        connection.addNode("testEdge1", 121, 122, "h1", "String", "String", "String", "String");
+        connection.addNode("testEdge2", 12, 15, "h1", "String", "String", "String", "String");
+        connection.addEdge("testEdge1_testEdge2", "testEdge1", "testEdge2");
+        // if this works, testResult should be 1
+        int testResult = connection.addEdge("testEdge1_testEdge2", "testEdge1", "testEdge2");
+
+        assertTrue(testResult == 0);
+    }
+
+    @Test
+    @DisplayName("testAddEdge: the startNode does not exist")
+    public void testAddEdge3(){
+        connection.addNode("testEdge1", 121, 122, "h1", "String", "String", "String", "String");
+        connection.addNode("testEdge2", 12, 15, "h1", "String", "String", "String", "String");
+
+        // if this works, testResult should be 1
+        int testResult = connection.addEdge("testEdge1_testEdge2", "testEdge3", "testEdge2");
+
+        assertTrue(testResult == 0);
+    }
+
+    @Test
+    @DisplayName("testAddEdge: the endNode does not exist")
+    public void testAddEdge4(){
+        connection.addNode("testEdge1", 121, 122, "h1", "String", "String", "String", "String");
+        connection.addNode("testEdge2", 12, 15, "h1", "String", "String", "String", "String");
+
+        // if this works, testResult should be 1
+        int testResult = connection.addEdge("testEdge1_testEdge2", "testEdge1", "testEdge3");
+
+        assertTrue(testResult == 0);
     }
 
     @Test
@@ -317,7 +538,7 @@ public class DatabaseTests {
         assertTrue(listOfNodeIDs.equals(connection.getListofNodeIDS()));
     }
 
-    //TODO: haven't done yet
+
     @Test
     @DisplayName("testGetNewCSVFile")
     public void testGetNewCSVFile(){

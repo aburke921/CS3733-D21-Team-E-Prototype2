55712385fb8d5605e51b701ce15590a0e139d09e
geekblack22 <geekblack22@users.noreply.github.com>
2021-04-16 12:01:29 -0400

    Added ability to store user input to SanitationServices #63

--- src/main/java/edu/wpi/TeamE/views/PathFinder.java
+++ src/main/java/edu/wpi/TeamE/views/PathFinder.java
@@ -94,8 +94,16 @@ public class PathFinder {
      * @param event calling event info.
      */
     @FXML
-    void selectStartNode(ActionEvent event) {
-        // findPath button validation
+    void selectStartNode(ActionEvent event) { //todo is only called if a different name is chosen from dropdown, won't update for duplicate names
+        String dropdownSelected = ((JFXComboBox) event.getSource()).getValue().toString();
+        System.out.println("\nselected start node name: " + dropdownSelected);
+
+        //get index and ID of selected item in dropdown
+        int startLocationListSelectedIndex = startLocationList.getSelectionModel().getSelectedIndex();
+        startNodeID = nodeIDArrayList.get(startLocationListSelectedIndex);
+        System.out.println("New ID resolution: (index) " + startLocationListSelectedIndex + ", (ID) " + startNodeID);
+
+        // findPath button validation todo, validation check for not having same start and end location
         if (startLocationList.getSelectionModel().isEmpty() ||
                 endLocationList.getSelectionModel().isEmpty()) {
             findPathButton.setDisable(true);
@@ -109,8 +117,16 @@ public class PathFinder {
      * @param event calling event info.
      */
     @FXML
-    void selectEndNode(ActionEvent event) {
-        // findPath button validation
+    void selectEndNode(ActionEvent event) { //todo is only called if a different name is chosen from dropdown, won't update for duplicate names
+        String dropdownSelected = ((JFXComboBox) event.getSource()).getValue().toString();
+        System.out.println("\nselected end node name: " + dropdownSelected);
+
+        //get index of selected item in dropdown
+        int endLocationListSelectedIndex = endLocationList.getSelectionModel().getSelectedIndex();
+        endNodeID = nodeIDArrayList.get(endLocationListSelectedIndex);
+        System.out.println("New ID resolution: (index) " + endLocationListSelectedIndex + ", (ID) " + endNodeID);
+
+        // findPath button validation todo, validation check for not having same start and end location
         if (startLocationList.getSelectionModel().isEmpty() ||
                 endLocationList.getSelectionModel().isEmpty()) {
             findPathButton.setDisable(true);
@@ -127,32 +143,29 @@ public class PathFinder {
     @FXML
     public void findPath(ActionEvent event) {
 
-        int startLocationListSelectedIndex = startLocationList.getSelectionModel().getSelectedIndex();
-        startNodeID = nodeIDArrayList.get(startLocationListSelectedIndex);
-        System.out.println("New ID resolution: (index) " + startLocationListSelectedIndex + ", (ID) " + startNodeID);
+        System.out.print("\nFINDING PATH...");
 
-        int endLocationListSelectedIndex = endLocationList.getSelectionModel().getSelectedIndex();
-        endNodeID = nodeIDArrayList.get(endLocationListSelectedIndex);
-        System.out.println("New ID resolution: (index) " + endLocationListSelectedIndex + ", (ID) " + endNodeID);
-
-        System.out.println("\nFINDING PATH...");
+        //Execute A* Search
+        System.out.println("A* Search with startNodeID of " + startNodeID + ", and endNodeID of " + endNodeID + "\n");
+        Searcher aStar = new AStarSearch();
 
         //Check if starting and ending node are the same
         if(startNodeID.equals(endNodeID)) {
             System.out.println("Cannot choose the same starting and ending location. Try again");
+            //Don't allow the program to call the path search function
+            findPathButton.setDisable(true);
 
-            //remove drawn line
-            pane.getChildren().clear();
+            BorderPane borderPane = new BorderPane();
+            Scene scene = new Scene(borderPane, 450, 100);
+            Text text = new Text("Cannot choose the same starting and ending location. Try again.");
+            borderPane.setCenter(text);
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.setTitle("Error");
+            stage.show();
 
-            //SnackBar popup
-            JFXSnackbar bar = new JFXSnackbar(pane);
-            bar.enqueue(new JFXSnackbar.SnackbarEvent(new JFXSnackbarLayout("Cannot choose the same starting and ending location.\nTry again.")));
         }
         else {
-            //Execute A* Search
-            System.out.print("A* Search with startNodeID of " + startNodeID + ", and endNodeID of " + endNodeID + "\n");
-            Searcher aStar = new AStarSearch();
-
             //Call the path search function
             Path foundPath = aStar.search(startNodeID, endNodeID);
 
--- src/main/java/edu/wpi/TeamE/views/SanitationServices.java
+++ src/main/java/edu/wpi/TeamE/views/SanitationServices.java
@@ -2,6 +2,7 @@ package edu.wpi.TeamE.views;
 
 import edu.wpi.TeamE.App;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -11,17 +12,26 @@ import java.io.IOException;
 public class SanitationServices extends ServiceRequestFormComponents {
 
 
-  @FXML
-  private TextField roomNumInput;
-  @FXML private TextField mnumInput;
-  @FXML private TextField mess;
-  @FXML private TextField doseMeasureInput;
+
+
+  @FXML private TextField numInput;
+  @FXML private TextField assignedIndividual;
   @FXML private TextArea sdetailedInstructionsInput;
   @FXML private TextField signatureInput;
 
   @FXML private MenuButton departmentInput;
   @FXML private MenuButton typeInput;
   @FXML private MenuButton ServiceTypeinput;
+  @FXML private Button submit;
+  public String assigneeName;
+  public String number;
+  public String  LocationType;
+  public String  serviceType;
+  public String department;
+  public String signature;
+  public String detailedDescription;
+
+
 
   /**
    * Sets value of drop down list to the selected value(departments)
@@ -30,6 +40,7 @@ public class SanitationServices extends ServiceRequestFormComponents {
   @FXML
   private void selectDepartment(ActionEvent actionEvent) {
     String department = ((MenuItem) actionEvent.getSource()).getText();
+    this.department = department;
     departmentInput.setText(department);
   }
   /**
@@ -39,6 +50,7 @@ public class SanitationServices extends ServiceRequestFormComponents {
   @FXML
   private void selectServiceType(ActionEvent actionEvent) {
     String service = ((MenuItem) actionEvent.getSource()).getText();
+    serviceType = service;
     ServiceTypeinput.setText(service);
   }
   /**
@@ -48,6 +60,21 @@ public class SanitationServices extends ServiceRequestFormComponents {
   @FXML
   private void SelectType(ActionEvent actionEvent) {
     String type = ((MenuItem) actionEvent.getSource()).getText();
+    LocationType = type;
     typeInput.setText(type);
   }
+  /**
+   * records inputs from user into a series of String variables and returns to the main page
+   * @param actionEvent
+   */
+  @FXML
+  private void saveData(ActionEvent actionEvent){
+    super.handleButtonSubmit(actionEvent);
+    assigneeName = assignedIndividual.getText();
+    number = numInput.getText();
+    System.out.println("Location: " + LocationType + " " + number + "\t" + "Assignee: " + assigneeName);
+
+
+  }
+
 }

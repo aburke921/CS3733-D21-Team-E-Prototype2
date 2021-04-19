797a935581373acfc76ec7db9d1fa81de3f3e25c
aburke921 <aburke@wpi.edu>
2021-04-19 18:15:29 -0400

    called the function that sets up all of the data in the App.java init()

--- src/main/java/edu/wpi/TeamE/App.java
+++ src/main/java/edu/wpi/TeamE/App.java
@@ -34,6 +34,7 @@ public class App extends Application {
 			connection.createTables();
 			connection.populateTable("node", nodes);
 			connection.populateTable("hasEdge", edges);
+			connection.addDataForPresentation();
 			System.out.println("Tables were created");
 		} catch (Exception e) {
 			System.out.println("Tables already there");

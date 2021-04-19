257416ce5345cffb5517317a6f7adb507ad415b9
RVRX <colemanning@yandex.com>
2021-04-19 14:15:44 -0400

    create App Bar Component controller+fxml

---
+++ src/main/java/edu/wpi/TeamE/views/AppBarComponent.java
@@ -0,0 +1,25 @@
+package edu.wpi.TeamE.views;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class AppBarComponent {
+
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML
+    void getHelpDefault(ActionEvent event) {
+
+    }
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+
+    }
+}

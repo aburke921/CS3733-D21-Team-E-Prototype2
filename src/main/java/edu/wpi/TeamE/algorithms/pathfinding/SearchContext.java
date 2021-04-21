470fdc18eed48f07fb7dab6e1e4458bd77ff48de
shanedonahue <skdonahue07@gmail.com>
2021-04-20 20:49:07 -0400

    built in composite constraints

--- src/main/java/edu/wpi/TeamE/algorithms/pathfinding/SearchContext.java
+++ src/main/java/edu/wpi/TeamE/algorithms/pathfinding/SearchContext.java
@@ -6,25 +6,42 @@ import edu.wpi.TeamE.algorithms.pathfinding.constraints.*;
 
 public class SearchContext {
     private Searcher search;
-    private SearchConstraint searchType;
-
-    public SearchContext(){
-        this(new Searcher(), "VANILLA");
-    }
+    private CompositeConstraint constraints;
 
     public SearchContext(Searcher _search, String _type){
         search = _search;
+        constraints = new CompositeConstraint();
         setConstraint(_type);
+    }
+
+    public SearchContext(){
+        this("VANILLA");
+    }
+
+    public SearchContext(String _type){
+        this(new Searcher(), _type);
     }
 
     public void setAlgo(Searcher newSearch){
         search = newSearch;
-        search.setType(searchType);
+        search.setType(constraints);
     }
 
     public void setConstraint(String _type){
-        searchType = translate(_type);
-        search.setType(searchType);
+        SearchConstraint constraint = translateConstraint(_type);
+        search.setType(constraint);
+    }
+
+    public void addConstraint(String _type){
+        SearchConstraint constraint = translateConstraint(_type);
+        constraints.add(constraint);
+        search.setType(constraints);
+    }
+
+    public void removeConstraint(String _type){
+        SearchConstraint constraint = translateConstraint(_type);
+        constraints.remove(constraint);
+        search.setType(constraint);
     }
 
     public Path search(String startNode, String endNode){
@@ -35,18 +52,28 @@ public class SearchContext {
         return search.searchAlongPath(route, stopType);
     }
 
-
-    private SearchConstraint translate(String type){
+    private SearchConstraint translateConstraint(String type){
         if(type.equalsIgnoreCase("SAFE")){
             return new SafeSearch();
         } else if(type.equalsIgnoreCase("HANDICAP")){
             return new HandicapSearch();
-        } else if(type.equalsIgnoreCase("VANILLA")){
+        } else if(type.equalsIgnoreCase("VANILLA")) {
             return new VanillaSearch();
         } else {
             return null;
         }
     }
+
+    private Searcher translateAlgo(String algo){
+        if(algo.equalsIgnoreCase("A*")){
+            return new Searcher();
+        } else if(algo.equalsIgnoreCase("DFS")){
+            return new DFSSearcher();
+        } else {
+            return null;
+        }
+    }
+
 
     public void refresh(){
         search.refreshGraph();

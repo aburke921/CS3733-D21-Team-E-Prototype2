85157ed93b587efde2a23d95aca34168f931668e
shanedonahue <skdonahue07@gmail.com>
2021-04-23 14:39:06 -0400

    made fields' access private

--- src/main/java/edu/wpi/TeamE/algorithms/scheduler/Event.java
+++ src/main/java/edu/wpi/TeamE/algorithms/scheduler/Event.java
@@ -3,10 +3,10 @@ package edu.wpi.TeamE.algorithms.scheduler;
 import edu.wpi.TeamE.algorithms.Node;
 
 public class Event implements Comparable<Event>{
-    Node location;
-    String description;
-    int arrivalTime;
-    int endTime;
+    private Node location;
+    private String description;
+    private int arrivalTime;
+    private int endTime;
 
     public Event(Node _location, String _description, int _arrivalTime, int _endTime){
         location = _location;

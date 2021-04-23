33a24215335d6aa1428fe17c96facc6de2f77b7d
shanedonahue <skdonahue07@gmail.com>
2021-04-23 17:55:50 -0400

    added time interval options

--- src/main/java/edu/wpi/TeamE/algorithms/user/User.java
+++ src/main/java/edu/wpi/TeamE/algorithms/user/User.java
@@ -1,11 +1,14 @@
 package edu.wpi.TeamE.algorithms.user;
 
+import edu.wpi.TeamE.algorithms.Node;
 import edu.wpi.TeamE.algorithms.Path;
+import edu.wpi.TeamE.algorithms.Time;
 import edu.wpi.TeamE.algorithms.pathfinding.SearchContext;
 import edu.wpi.TeamE.algorithms.scheduler.DailySchedule;
 import edu.wpi.TeamE.algorithms.scheduler.Event;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class User {
@@ -33,15 +36,50 @@ public class User {
     }
 
     public Path drawSchedule(SearchContext search){
-        return search.search(schedule.getLocations());
+        return search.search(getLocations());
+    }
+
+    public Path drawSchedule(SearchContext search, int startTime, int endTime){
+        return search.search(getLocations(startTime, endTime));
+    }
+
+    public List<String> listSchedule(int startTime, int endTime){
+        List<String> nodes = new ArrayList<>(schedule.size());
+        Iterator<Event> itr = schedule.iterator(startTime, endTime);
+        while(itr.hasNext()){
+            nodes.add(itr.next().toString());
+        }
+        return nodes;
     }
 
     public List<String> listSchedule(){
-        List<String> events = new ArrayList<>();
-        for(Event event : schedule){
-            events.add(event.toString());
+        return listSchedule(Time.DAY_START, Time.DAY_END);
+    }
+
+    public List<Node> getLocations(int startTime, int endTime){
+        List<Node> nodes = new ArrayList<>(schedule.size());
+        Iterator<Event> itr = schedule.iterator(startTime, endTime);
+        while(itr.hasNext()){
+            nodes.add(itr.next().getLocation());
         }
-        return events;
+        return nodes;
+    }
+
+    public List<Node> getLocations(){
+        return getLocations(Time.DAY_START, Time.DAY_END);
+    }
+
+    public List<Time> getArrivalTimes(int startTime, int endTime){
+        List<Time> nodes = new ArrayList<>(schedule.size());
+        Iterator<Event> itr = schedule.iterator(startTime, endTime);
+        while(itr.hasNext()){
+            nodes.add(new Time(itr.next().getArrivalTime()));
+        }
+        return nodes;
+    }
+
+    public List<Time> getArrivalTimes(){
+        return getArrivalTimes(Time.DAY_START, Time.DAY_END);
     }
 
     public List<Event> getEvents(){
@@ -49,11 +87,13 @@ public class User {
     }
 
     public Path whereToNext(SearchContext search, int currentTime){
+        Iterator<Event> itr = schedule.iterator();
+        while(itr.hasNext()){
+            Event event = itr.next();
+            if(itr.hasNext() && event.getArrivalTime() < currentTime && event.getEndTime() < currentTime){
+                return search.search(event.getLocation(), itr.next().getLocation());
+            }
+        }
         return null;
     }
-
-    public Event nextEvent(int currentTime){
-        return null;
-    }
-
 }

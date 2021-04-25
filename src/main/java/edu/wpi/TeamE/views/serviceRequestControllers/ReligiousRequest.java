54220b728d1d0f94cd33bf66f000715cf94033bc
aidanmulcahey <ammulcahey@wpi.edu>
2021-04-25 17:31:32 -0400

    Commit for Service Request

--- src/main/java/edu/wpi/TeamE/views/serviceRequestControllers/ReligiousRequest.java
+++ src/main/java/edu/wpi/TeamE/views/serviceRequestControllers/ReligiousRequest.java
@@ -1,4 +1,42 @@
 package edu.wpi.TeamE.views.serviceRequestControllers;
 
-public class ReligiousRequest {
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+
+
+import javafx.event.ActionEvent;
+
+public class ReligiousRequest extends ServiceRequestFormComponents {
+
+    @FXML
+    private JFXComboBox<String> locationInput;
+
+    @FXML
+    private JFXComboBox<String> religionInput;
+
+    @FXML
+    private JFXTextField assignedPersonnel;
+
+    @FXML
+    private JFXTextArea description;
+
+    @FXML
+    private JFXButton submit;
+
+    @FXML
+    private JFXButton cancel;
+
+    @FXML
+    void handleButtonSubmit(ActionEvent event) {
+        super.handleButtonSubmit(event);
+    }
+
+    @FXML
+    void handleButtonCancel(ActionEvent event) {
+        super.handleButtonCancel(event);
+    }
+
 }

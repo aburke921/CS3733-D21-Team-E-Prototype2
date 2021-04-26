dbc51e2353e52e6adf79aab13ff6f51a866cfd01
Matthew Haahr <matthew.haahr@gmail.com>
2021-04-25 23:28:59 -0400

    Visual Improvements #189

--- src/main/java/edu/wpi/TeamE/views/PathFinder.java
+++ src/main/java/edu/wpi/TeamE/views/PathFinder.java
@@ -447,22 +447,25 @@ public class PathFinder {
                     double yCoord = (double) node.getY() / scale;
 
                     if (firstNode == 1) { //if current node is the starting node
-                        firstNode = 0;
+                        if (!(prevYCoord < 1) || !(prevXCoord < 1)) {
+                            //technically second node, here to prevent circle from being "under" path line, prev will be fist node
+                            firstNode = 0;
+                            Circle circle;
+                            circle = new Circle(prevXCoord, prevYCoord, radius, Color.GREEN);
+
+                            //create a line between this node and the previous node
+                            Line line = new Line(prevXCoord, prevYCoord, xCoord, yCoord);
+                            line.setStrokeLineCap(StrokeLineCap.ROUND);
+                            line.setStrokeWidth(strokeWidth);
+                            line.setStroke(Color.RED);
+
+                            g.getChildren().addAll(line, circle);
+                        }
+                        //update the coordinates for the previous node
                         prevXCoord = xCoord;
                         prevYCoord = yCoord;
 
-                        if (node.get("id").equals(selectedStartNodeID)) { // start node of entire path
 
-                            //place a dot on the location
-                            Circle circle = new Circle(xCoord, yCoord, radius, Color.GREEN);
-                            g.getChildren().add(circle);
-                        } else { // start node of just this floor
-
-                            //place a red dot on the location
-                            Circle circle = new Circle(xCoord, yCoord, radius, Color.RED);
-                            g.getChildren().add(circle);
-
-                        }
                     } else if (!legItr.hasNext()) { //if current node is the ending node for this floor
 
                         Circle circle;
@@ -481,7 +484,7 @@ public class PathFinder {
                         line.setStrokeWidth(strokeWidth);
                         line.setStroke(Color.RED);
 
-                        g.getChildren().addAll(circle, line);
+                        g.getChildren().addAll(line, circle);
                     } else {
                         //create a line between this node and the previous node
                         Line line = new Line(prevXCoord, prevYCoord, xCoord, yCoord);

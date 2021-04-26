3fc3a668d1025ece886e5c16258cea216e2d3297
RVRX <colemanning@yandex.com>
2021-04-25 22:07:03 -0400

    Add AppBar to CovidSurvey page #168

--- src/main/java/edu/wpi/TeamE/views/CovidSurvey.java
+++ src/main/java/edu/wpi/TeamE/views/CovidSurvey.java
@@ -7,11 +7,16 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
 
 
 import java.io.IOException;
 
 public class CovidSurvey extends ServiceRequests  {
+
+    @FXML private AnchorPane appBarAnchorPane;
+    @FXML private StackPane stackPane;
     @FXML JFXCheckBox positiveTest;
     @FXML JFXCheckBox symptoms;
     @FXML JFXCheckBox closeContact;
@@ -39,6 +44,21 @@ public class CovidSurvey extends ServiceRequests  {
 
     @FXML
     public void initialize() {
+
+        //init appBar
+        javafx.scene.Node appBarComponent = null;
+        try {
+            App.setShowLogin(true);
+            App.setShowHelp(true);
+            App.setPageTitle("Covid Survey"); //set AppBar title
+            App.setHelpText(""); //set help text todo, fill in this field
+            App.setStackPane(stackPane); // required for dialog boxes
+            appBarComponent = FXMLLoader.load(getClass().getResource("/edu/wpi/TeamE/fxml/AppBarComponent.fxml"));
+            appBarAnchorPane.getChildren().add(appBarComponent); //add FXML to this page's anchorPane element
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         assert  positiveTest  != null : "fx:id=\"positiveTest\" was not injected: check your FXML file 'PathFinder.fxml'.";
         assert  symptoms  != null : "fx:id=\"symptoms\" was not injected: check your FXML file 'PathFinder.fxml'.";
         assert closeContact  != null : "fx:id=\"closeContact\" was not injected: check your FXML file 'PathFinder.fxml'.";

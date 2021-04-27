ec00e6b34fe3a47be3fa030d6c12c5b4e8128430
aburke921 <aburke@wpi.edu>
2021-04-27 07:36:11 -0400

    added MenuPage controller Tree table outline

--- src/main/java/edu/wpi/TeamE/views/MenuPage.java
+++ src/main/java/edu/wpi/TeamE/views/MenuPage.java
@@ -2,17 +2,21 @@ package edu.wpi.TeamE.views;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTreeTableView;
+import edu.wpi.TeamE.App;
 import edu.wpi.TeamE.views.serviceRequestObjects.ServiceRequestForm;
+import edu.wpi.cs3733.D21.teamE.DB;
 import javafx.beans.property.ReadOnlyStringWrapper;
 import javafx.fxml.FXML;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeTableColumn;
 import javafx.scene.control.TreeTableView;
 
+import java.util.ArrayList;
+
 public class MenuPage {
 
     @FXML
-    private JFXTreeTableView<> menuTable;
+    private JFXTreeTableView menuTable;
 
     @FXML
     private JFXButton cancelButton;
@@ -49,6 +53,61 @@ public class MenuPage {
         }
         //Establishing root node
         TreeItem<ServiceRequestForm> rootNode = new TreeItem<>(new ServiceRequestForm("Service Requests"));
+    }
+
+    /**
+     * This function populates a specific part of the table from the database
+     *
+     * @param tableName the name of the table the data is coming from
+     * @param inProgress the TreeItem for the service requests still in progress
+     * @param completed the TreeItem for the service requests that have been completed
+     * @param cancelled the TreeItem for the service requests that were cancelled
+     */
+    private void addToTable(String tableName, TreeItem<ServiceRequestForm> inProgress, TreeItem<ServiceRequestForm> completed, TreeItem<ServiceRequestForm> cancelled) {
+
+        ArrayList<String> idArray = DB.getMyCreatedRequestInfo(tableName, App.userID, "requestID");
+//        for(int j = 0; j < idArray.size(); j++) {
+//            System.out.println(idArray.get(j));
+//        }
+        ArrayList<String> statusArray = DB.getMyCreatedRequestInfo(tableName, App.userID, "requestStatus");
+        ArrayList<String> locationArray = DB.getRequestLocations(tableName, App.userID);
+        ArrayList<String> assigneeArray = DB.getMyCreatedRequestInfo(tableName, App.userID, "assigneeID");
+        if(idArray.size() > 0) {
+            System.out.println("Array size" + idArray.size());
+            if (!inProgress.getChildren().isEmpty()) {
+                removeChildren(inProgress);
+            }
+            if (!completed.getChildren().isEmpty()) {
+                removeChildren(completed);
+            }
+            if (!cancelled.getChildren().isEmpty()) {
+                removeChildren(cancelled);
+            }
+            for (int i = 0; i < idArray.size(); i++) {
+                System.out.println("Before");
+                TreeItem<ServiceRequestForm> request = new TreeItem<>(new ServiceRequestForm(idArray.get(i), locationArray.get(i), assigneeArray.get(i), statusArray.get(i)));
+                System.out.println(request.getValue().getId());
+                if (request.getValue().getStatus().equals("inProgress")) {
+                    inProgress.getChildren().add(request);
+                }
+                if (request.getValue().getStatus().equals("complete")) {
+                    completed.getChildren().add(request);
+                }
+                if (request.getValue().getStatus().equals("canceled")) {
+                    cancelled.getChildren().add(request);
+                }
+            }
+        }
+    }
+
+    public void removeChildren(TreeItem<ServiceRequestForm> treeItem) {
+        int removal = treeItem.getChildren().size();
+        System.out.println(removal);
+        if(treeItem.getChildren().size() != 0) {
+            treeItem.getChildren().remove(0,removal);
+        }
+        TreeItem<ServiceRequestForm> test = treeItem;
+        System.out.println(test.getChildren().size());
     }
 
 

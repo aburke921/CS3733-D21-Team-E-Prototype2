3bd07a35de6dc0b6a8e771d14ca88e635acc572b
GP2P <73323107+GP2P@users.noreply.github.com>
2021-04-26 22:50:30 -0400

    WE GOT QR READING WORKING!!!!!!!!

--- src/main/java/edu/wpi/cs3733/D21/teamE/QRCode.java
+++ src/main/java/edu/wpi/cs3733/D21/teamE/QRCode.java
@@ -1,35 +1,43 @@
 package edu.wpi.cs3733.D21.teamE;
 
-import java.io.File;
+import com.github.sarxos.webcam.Webcam;
+import com.github.sarxos.webcam.WebcamUtils;
+import com.github.sarxos.webcam.util.ImageUtils;
+import com.google.zxing.*;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.common.HybridBinarizer;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+
+import javax.imageio.ImageIO;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import javax.imageio.ImageIO;
-import com.google.zxing.BarcodeFormat;
-import com.google.zxing.BinaryBitmap;
-import com.google.zxing.EncodeHintType;
-import com.google.zxing.MultiFormatReader;
-import com.google.zxing.MultiFormatWriter;
-import com.google.zxing.NotFoundException;
-import com.google.zxing.Result;
-import com.google.zxing.WriterException;
-import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
-import com.google.zxing.client.j2se.MatrixToImageWriter;
-import com.google.zxing.common.BitMatrix;
-import com.google.zxing.common.HybridBinarizer;
-import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
 public class QRCode {
 
 
+	public static String scanQR() {
+		Webcam webcam = Webcam.getDefault();
+		String result = null;
+		Map<EncodeHintType, ErrorCorrectionLevel> hintMap = new HashMap<>();
+
+		hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
+
+		while (result == null) {
+			WebcamUtils.capture(webcam, "src/main/resources/edu/wpi/TeamE/temp/temp", ImageUtils.FORMAT_PNG);
+			try {
+				result = QRCode.readQR("src/main/resources/edu/wpi/TeamE/temp/temp.png", "UTF-8", hintMap);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return result;
+	}
+
 	// Function to read the QR file
-	public static String readQR(String path, String charset,
-	                            Map hashMap)
-			throws FileNotFoundException, IOException,
-			NotFoundException
-	{
+	public static String readQR(String path, String charset, Map hashMap) throws FileNotFoundException, IOException {
 		BinaryBitmap binaryBitmap
 				= new BinaryBitmap(new HybridBinarizer(
 				new BufferedImageLuminanceSource(
@@ -37,33 +45,36 @@ public class QRCode {
 								new FileInputStream(path)))));
 
 		Result result
-				= new MultiFormatReader().decode(binaryBitmap);
+				= null;
+		try {
+			result = new MultiFormatReader().decode(binaryBitmap);
+		} catch (NotFoundException e) {
+			//e.printStackTrace();
+			return null;
+		}
 
 		return result.getText();
 	}
 
 	// Driver code
-	public static void main(String[] args)
-			throws WriterException, IOException,
-			NotFoundException
-	{
+	public static void main(String[] args) throws IOException {
 
 		// Path where the QR code is saved
-		String filePath = "src/main/resources/edu/wpi/TeamE/temp/";
+		String filePathTrue = "src/main/resources/edu/wpi/TeamE/temp/qrcode_www.geeksforgeeks.org.png";
+		String filePathFake = "src/main/resources/edu/wpi/TeamE/temp/FakeQR.png";
 
 		// Encoding charset
 		String charset = "UTF-8";
 
 		Map<EncodeHintType, ErrorCorrectionLevel> hintMap
-				= new HashMap<EncodeHintType,
-				ErrorCorrectionLevel>();
+				= new HashMap<EncodeHintType, ErrorCorrectionLevel>();
 
-		hintMap.put(EncodeHintType.ERROR_CORRECTION,
-				ErrorCorrectionLevel.L);
+		hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
 
-		System.out.println(
-				"QRCode output: "
-						+ readQR(filePath, charset, hintMap));
+		System.out.println("QRCode output: " + readQR(filePathTrue, charset, hintMap));
+		System.out.println("QRCode output: " + readQR(filePathFake, charset, hintMap));
+		System.out.println(scanQR());
 	}
+
 
 }

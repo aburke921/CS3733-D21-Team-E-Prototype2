3a9148ba6a5cd716e826bdc6279fee610a711a0b
nshukla2019 <nupi.shukla@gmail.com>
2021-04-26 23:42:46 -0400

    added religiousRequest fncs + Junit Tests to this branch

--- src/main/java/edu/wpi/TeamE/App.java
+++ src/main/java/edu/wpi/TeamE/App.java
@@ -161,6 +161,7 @@ public class App extends Application {
 				DB.createFoodDeliveryTable();
 				DB.createAubonPainMenuTable();
 				DB.createInternalPatientRequest();
+				DB.createReligionRequestTable();
 				DB.populateTable("node", nodes);
 				DB.populateTable("hasEdge", edges);
 				connection.addDataForPresentation();

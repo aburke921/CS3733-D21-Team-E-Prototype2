def9fcf68277cb0555da8bb732b625ee24572b0d
Shannen <sklin@wpi.edu>
2021-04-30 15:55:57 -0400

    added state classes

---
+++ src/main/java/edu/wpi/cs3733/D21/teamE/states/CovidSurveyState.java
@@ -0,0 +1,10 @@
+package edu.wpi.cs3733.D21.teamE.states;
+
+import javafx.event.ActionEvent;
+
+public class CovidSurveyState implements State{
+
+    public void switchScene(ActionEvent event) {
+
+    }
+}
---
+++ src/main/java/edu/wpi/cs3733/D21/teamE/states/UserManagementState.java
@@ -0,0 +1,4 @@
+package edu.wpi.cs3733.D21.teamE.states;
+
+public class UserManagementState {
+}

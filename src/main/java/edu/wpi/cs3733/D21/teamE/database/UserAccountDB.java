0b90987d7c94c07c08ba29dcca9b161982f0a491
GP2P <73323107+GP2P@users.noreply.github.com>
2021-04-30 02:18:43 -0400

    Removed redundant code

--- src/main/java/edu/wpi/cs3733/D21/teamE/database/UserAccountDB.java
+++ src/main/java/edu/wpi/cs3733/D21/teamE/database/UserAccountDB.java
@@ -348,24 +348,5 @@ public class UserAccountDB {
 			System.err.println("Error in filledCovidSurveyToday() from UserAccountDB");
 		}
 		return filledToday;
-
-//		Timestamp ts = new Timestamp(System.currentTimeMillis());
-//		Date date = new Date(ts.getTime());
-//		System.out.println(date);
-//
-//		SimpleDateFormat sdformat = new SimpleDateFormat("yyyy-MM-dd");
-//		Date d1 = sdformat.parse("2019-04-15");
-//		Date d2 = sdformat.parse("2019-08-10");
-//		System.out.println("The date 1 is: " + sdformat.format(d1));
-//		System.out.println("The date 2 is: " + sdformat.format(d2));
-//		if(d1.compareTo(d2) > 0) {
-//			System.out.println("Date 1 occurs after Date 2");
-//		} else if(d1.compareTo(d2) < 0) {
-//			System.out.println("Date 1 occurs before Date 2");
-//		} else if(d1.compareTo(d2) == 0) {
-//			System.out.println("Both dates are equal");
-//		}
-//
-//		"Select lastCovidSurveyTime From userAccount Where USERID = ?"
 	}
 }

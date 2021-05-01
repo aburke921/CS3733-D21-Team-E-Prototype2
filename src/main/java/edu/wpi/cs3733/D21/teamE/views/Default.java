df2e817938979df731a6212aa5355079beb1a531
GP2P <73323107+GP2P@users.noreply.github.com>
2021-05-01 10:20:04 -0400

    Added parking info display to Default page's UI
    for #218 #221 #222

--- src/main/java/edu/wpi/cs3733/D21/teamE/views/Default.java
+++ src/main/java/edu/wpi/cs3733/D21/teamE/views/Default.java
@@ -13,6 +13,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -53,6 +54,12 @@ public class Default {
 
     @FXML // fx:id="algoTextBottom"
     private Label algoTextBottom;
+
+    @FXML // fx:id="carParkedText"
+    private Label carParkedText;
+
+    @FXML // fx:id="LinkToParking"
+    private Hyperlink LinkToParking;
 
     private ObservableList<String> algoNames;
 

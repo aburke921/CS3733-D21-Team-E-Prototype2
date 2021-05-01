10f1c19942ad6b03a7c23df73894345fe1ca6d08
nshukla2019 <nupi.shukla@gmail.com>
2021-05-01 13:33:37 -0400

    updating sheet when user edits appointment #253

--- src/main/java/edu/wpi/cs3733/D21/teamE/email/SheetsAndJava.java
+++ src/main/java/edu/wpi/cs3733/D21/teamE/email/SheetsAndJava.java
@@ -6,23 +6,19 @@ import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.client.util.Value;
 import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.sheets.v4.Sheets;
 import com.google.api.services.sheets.v4.SheetsScopes;
-import com.google.api.services.sheets.v4.model.AppendValuesResponse;
-import com.google.api.services.sheets.v4.model.ValueRange;
+import com.google.api.services.sheets.v4.model.*;
 
-import java.io.FileNotFoundException;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.security.GeneralSecurityException;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class SheetsAndJava {
@@ -59,13 +55,105 @@ public class SheetsAndJava {
                 .setApplicationName(APPLICATION_NAME)
                 .build();
     }
-    public static void editSheet() throws IOException, GeneralSecurityException {
+
+
+    public static int getRow(int appointmentID) throws IOException, GeneralSecurityException {
+        sheetsService = getSheetService();
+        int rowCounter = 0;
+        String range = "Sheet1";
+
+        ValueRange response = sheetsService.spreadsheets().values()
+                .get(SPREADSHEET_ID, range)
+                .execute();
+
+        List<List<Object>> values = response.getValues();
+
+        for (List row : values) {
+            rowCounter++;
+            if (row.get(0).equals(appointmentID)){
+                System.out.println("Row Num:" + rowCounter);
+                break;
+            }
+        }
+        return rowCounter - 1;
+    }
+
+
+    // get date from column Date to check if reminder has been sent
+        // if reminder has been sent
+        // update any columns user has changed (need fncs for each of these)
+        // delete Date and update it with the same date (or changed date)
+    // if reminder has NOT been sent
+        // update any columns user has changed
+//
+//    public static void updateStartTime(int row, String newStartTime) throws IOException, GeneralSecurityException {
+//
+//        sheetsService = getSheetService();
+//
+//        ValueRange body = new ValueRange()
+//                .setValues(Arrays.asList(
+//                        Arrays.asList(newStartTime)
+//                ));
+//
+//        UpdateValuesResponse result = sheetsService.spreadsheets().values()
+//                .update(SPREADSHEET_ID, "E"+row, body)
+//                .setValueInputOption("RAW")
+//                .execute();
+//    }
+//
+//    public static void updateDoctor(int row, String doctor) throws IOException, GeneralSecurityException {
+//
+//        sheetsService = getSheetService();
+//
+//        ValueRange body = new ValueRange()
+//                .setValues(Arrays.asList(
+//                        Arrays.asList(doctor)
+//                ));
+//
+//        UpdateValuesResponse result = sheetsService.spreadsheets().values()
+//                .update(SPREADSHEET_ID, "F"+row, body)
+//                .setValueInputOption("RAW")
+//                .execute();
+//    }
+
+    public static void deleteRow(int row) throws IOException, GeneralSecurityException {
+
+        sheetsService = getSheetService();
+
+        DeleteDimensionRequest deleteDateCell = new DeleteDimensionRequest()
+                .setRange(
+                        new DimensionRange()
+                        .setSheetId(0)
+                        .setDimension("ROWS")
+                        .setStartIndex(row-1)
+                        .setEndIndex(row)
+                );
+
+        List<Request> requests = new ArrayList<>();
+        requests.add(new Request().setDeleteDimension(deleteDateCell));
+
+        BatchUpdateSpreadsheetRequest body = new BatchUpdateSpreadsheetRequest().setRequests(requests);
+
+        sheetsService.spreadsheets().batchUpdate(SPREADSHEET_ID, body).execute();
+
+//        ValueRange body1 = new ValueRange()
+//                .setValues(Arrays.asList(
+//                        Arrays.asList(date)
+//                ));
+//
+//        UpdateValuesResponse result = sheetsService.spreadsheets().values()
+//                .update(SPREADSHEET_ID, "G"+row, body1)
+//                .setValueInputOption("RAW")
+//                .execute();
+    }
+
+    public static void addAppointmenttoSheet(int appointmentID, String email, String firstName, String lastName, String doctor, String date) throws IOException, GeneralSecurityException {
 
         sheetsService = getSheetService();
 
         ValueRange appendBody = new ValueRange()
                 .setValues(Arrays.asList(
-                        Arrays.asList("aburke@wpi.edu", "Ashley", "Burke", "05/01/21")
+                        Arrays.asList(appointmentID, email, firstName, lastName, doctor, date)
                 ));
 
         AppendValuesResponse appendResult = sheetsService.spreadsheets().values()
@@ -77,10 +165,16 @@ public class SheetsAndJava {
 
     }
 
-    public static void main(String[] args) throws IOException, GeneralSecurityException {
-        editSheet();
-    }
 
+
+    public static void main(String[] args) throws IOException, GeneralSecurityException {
+        //editSheet();
+        int rowNum = getRow(2);
+
+        //deleteRow(rowNum);
+
+        addAppointmenttoSheet(3,"nupi.shukla@gmail.com", "me", "test me", "me", "05/01/21 6:00");
+    }
 
 
 
--- src/main/java/edu/wpi/cs3733/D21/teamE/email/email.java
+++ src/main/java/edu/wpi/cs3733/D21/teamE/email/email.java
@@ -13,18 +13,15 @@ import javax.mail.internet.*;
 
 
 
-public class email{
+public class email {
 
-	//Timestamp startTime = new Timestamp(121, 3, 30, 21, 18, 30, 0);
 
 	public static void main(String args[]) throws MessagingException {
-		Date startTime = new Date();
-		startTime.setTime(startTime.getTime() + 60000);
+
 
 //		String email = DB.getEmail(App.userID);
 		//sendAppointmentRemind("aburke@wpi.edu", "May 5th 2021");
 
-		confirmationEmail(startTime);
 
 	}
 
@@ -44,26 +41,41 @@ public class email{
 	}
 
 
-	public static void sendEmail(String email, String subject, String emailBody) throws MessagingException{
+	// call this when user edits an appointment
+	public static void sendUpdatedAppointment(String email, String time) throws MessagingException {
+
+		String subject = "BWH Appointment Changed!!";
+		String emailMessage = "Hello " + email + ", \n\n"
+				+ "Your appointment details at Brigham & Women's hospital have been updated. If you have any issues with your "
+				+ "appointment please log into the application and edit your appointment information. \n\n"
+				+ "We look forward to seeing you soon, \n"
+				+ "Emerald Emus BWH";
+
+		sendEmail(email, subject, emailMessage);
+
+	}
+
+
+	public static void sendEmail(String email, String subject, String emailBody) throws MessagingException {
 		//Sets up getting permission to send email
 		String password;
 
 
-		if(System.getProperty("os.name").substring(0,3).equals("Mac")){
+		if (System.getProperty("os.name").substring(0, 3).equals("Mac")) {
 			password = "olpqmnwwpkhihwjs";
-		}
-		else{
+		} else {
 			password = "bwpeledauxsuxqja";
 		}
 
 		Properties props = System.getProperties();
-		props.put("mail.smtps.host","smtp.gmail.com");
-		props.put("mail.smtps.auth","true");
+		props.put("mail.smtps.host", "smtp.gmail.com");
+		props.put("mail.smtps.auth", "true");
 		Session session = Session.getInstance(props, null);
 
 		//Email information
 		Message msg = new MimeMessage(session);
-		msg.setFrom(new InternetAddress("engineeringsoftware3733@gmail.com"));;
+		msg.setFrom(new InternetAddress("engineeringsoftware3733@gmail.com"));
+		;
 		msg.setRecipients(Message.RecipientType.TO,
 				InternetAddress.parse(email, false));
 		msg.setSubject(subject);
@@ -71,8 +83,7 @@ public class email{
 		msg.setSentDate(new Date());
 
 
-
-		SMTPTransport t = (SMTPTransport)session.getTransport("smtps");
+		SMTPTransport t = (SMTPTransport) session.getTransport("smtps");
 
 		t.connect("smtp.gmail.com", "engineeringsoftware3733@gmail.com", password);
 		t.sendMessage(msg, msg.getAllRecipients());
@@ -81,31 +92,6 @@ public class email{
 		t.close();
 	}
 
-	public static void confirmationEmail(Date startTime) {
-
-		try {
-			JobDetail job = JobBuilder.newJob(AppointmentJob.class)
-					.withIdentity("AppointmentJob", "group1").build();
-
-			SimpleTrigger trigger = (SimpleTrigger) TriggerBuilder.newTrigger()
-					.withIdentity("trigger1", "group1")
-					.startAt(startTime)
-					.forJob("AppointmentJob", "group1")
-					.build();
-
-
-			Scheduler scheduler1 = new StdSchedulerFactory().getScheduler();
-			scheduler1.start();
-			scheduler1.scheduleJob(job, trigger);
-
-			Thread.sleep(100000);
-
-			scheduler1.shutdown();
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
 }
 
 

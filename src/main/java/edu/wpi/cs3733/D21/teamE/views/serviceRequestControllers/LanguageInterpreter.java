8840b5cb117b86cc295bb180a182829ca8341ab2
Seamus Sullivan <sheadog131313@gmail.com>
2021-05-01 11:54:11 -0400

    Updated Language Interpreter Added submission to database to language interpreter

--- src/main/java/edu/wpi/cs3733/D21/teamE/views/serviceRequestControllers/LanguageInterpreter.java
+++ src/main/java/edu/wpi/cs3733/D21/teamE/views/serviceRequestControllers/LanguageInterpreter.java
@@ -3,6 +3,8 @@ package edu.wpi.cs3733.D21.teamE.views.serviceRequestControllers;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.validation.RequiredFieldValidator;
+import edu.wpi.cs3733.D21.teamE.App;
 import edu.wpi.cs3733.D21.teamE.DB;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -52,15 +54,31 @@ public class LanguageInterpreter extends ServiceRequestFormComponents {
 		super.handleButtonSubmit(event);
 	}
 
+	private boolean validateInput() {
+		RequiredFieldValidator validator = new RequiredFieldValidator();
+
+		locationInput.getValidators().add(validator);
+		languageSelection.getValidators().add(validator);
+		assignedPersonnel.getValidators().add(validator);
+		descriptionInput.getValidators().add(validator);
+
+		return  locationInput.validate() && languageSelection.validate()
+				&& assignedPersonnel.validate() && descriptionInput.validate();
+	}
+
 	@FXML
 	void saveData(ActionEvent event) {
-		int index = locationInput.getSelectionModel().getSelectedIndex();
-		String node = nodeID.get(index);
-		String assignee = assignedPersonnel.getSelectionModel().getSelectedItem();
-		String descrip = descriptionInput.getText();
-		String language = languageSelection.getSelectionModel().getSelectedItem();
+		if(validateInput()) {
+			int userIndex = assignedPersonnel.getSelectionModel().getSelectedIndex();
+			int index = locationInput.getSelectionModel().getSelectedIndex();
+			int assignee = userID.get(userIndex);
+			String node = nodeID.get(index);
+			String descrip = descriptionInput.getText();
+			String language = languageSelection.getSelectionModel().getSelectedItem();
 
-		super.handleButtonSubmit(event);
+			DB.addLanguageRequest(App.userID, assignee, node, language, descrip);
+			super.handleButtonSubmit(event);
+		}
 	}
 
 	@FXML

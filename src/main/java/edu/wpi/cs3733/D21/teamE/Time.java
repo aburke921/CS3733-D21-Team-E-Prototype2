9340ce90b5dad7645026f1cd63a12b417d2b4e06
shanedonahue <skdonahue07@gmail.com>
2021-05-02 11:42:29 -0400

    added hours to time

--- src/main/java/edu/wpi/cs3733/D21/teamE/Time.java
+++ src/main/java/edu/wpi/cs3733/D21/teamE/Time.java
@@ -1,13 +1,13 @@
 package edu.wpi.cs3733.D21.teamE;
+
 public class Time {
 
     public static final int DAY_START = 0;
     public static final int DAY_END = 24 * 60 * 60 - 1; //11:59:59 in seconds
 
-
     private int sec;
     private int min;
-    private int hours;
+    private int hour;
 
     /**
      * Constructor with already known minutes and seconds
@@ -17,6 +17,13 @@ public class Time {
     public Time(int _min, int _sec){
         this.min = _min;
         this.sec = _sec;
+        this.hour = 0;
+    }
+
+    public Time(int _hour, int _min, int _sec){
+        this.hour = _hour;
+        this.min = _min;
+        this.sec = _sec;
     }
 
     /**
@@ -24,10 +31,12 @@ public class Time {
      * @param _sec Seconds
      */
     public Time(int _sec){
-        int sec = _sec % 60;
-        int min = _sec / 60;
-        this.sec = sec;
+        int hour = _sec / 60 / 60;
+        int min = _sec / 60 % 60;
+        int sec = _sec % 60 % 60;
+        this.hour = hour;
         this.min = min;
+        this.sec = sec;
     }
 
     /**
@@ -36,10 +45,7 @@ public class Time {
      */
     @Override
     public String toString() {
-        String mins = Integer.toString(min);
-        String secs = String.format("%02d", sec);
-
-        return (mins + ":" + secs);
+        return (hour + ":" + min + ":" + sec);
     }
 
     /**
@@ -66,19 +72,32 @@ public class Time {
         this.min = min;
     }
 
+    public int getHour(){
+        return hour;
+    }
+
+    public void setHour(int _hour){
+        hour = _hour;
+    }
+
     /**
      * Total seconds setter
      * @param secs Seconds
      */
     public void setTime(int secs){
-        int sec = secs % 60;
-        int min = secs / 60;
-        this.sec = sec;
+        int hour = secs / 60 / 60;
+        int min = secs / 60 % 60;
+        int sec = secs % 60 % 60;
+        this.hour = hour;
         this.min = min;
+        this.sec = sec;
+    }
+
+    public int getTotalSeconds(){
+        return hour*60*60+min*60+sec;
     }
 
     public boolean equals(Time t){
-        return ((t.getMin() == min) && (t.getSec() == sec));
-
+        return ((t.getHour() == hour) && (t.getMin() == min) && (t.getSec() == sec));
     }
 }

1d50dbfcae551e43624f4041e6259ace57b6fcfe
Cole Manning // RVRX <colemanning@yandex.com>
2021-05-02 12:31:39 -0400

    Fix ReligiousRequest appBar

--- src/main/java/edu/wpi/cs3733/D21/teamE/views/serviceRequestControllers/ReligiousRequest.java
+++ src/main/java/edu/wpi/cs3733/D21/teamE/views/serviceRequestControllers/ReligiousRequest.java
@@ -3,16 +3,17 @@ package edu.wpi.cs3733.D21.teamE.views.serviceRequestControllers;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextArea;
-import com.jfoenix.controls.JFXTextField;
 import com.jfoenix.validation.RequiredFieldValidator;
 import edu.wpi.cs3733.D21.teamE.App;
 import edu.wpi.cs3733.D21.teamE.DB;
 import javafx.collections.ObservableList;
-import javafx.fxml.FXML;
-
-
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class ReligiousRequest extends ServiceRequestFormComponents {
@@ -40,6 +41,12 @@ public class ReligiousRequest extends ServiceRequestFormComponents {
 
     @FXML
     private JFXButton cancel;
+
+    @FXML
+    private StackPane stackPane;
+
+    @FXML
+    private AnchorPane appBarAnchorPane;
 
     private boolean validateInput() {
         RequiredFieldValidator validator = new RequiredFieldValidator();
@@ -79,6 +86,21 @@ public class ReligiousRequest extends ServiceRequestFormComponents {
 
     @FXML
     void initialize() {
+
+        //init appBar
+        javafx.scene.Node appBarComponent = null;
+        try {
+            App.setShowHelp(false); // show help or not
+            App.setShowLogin(true); // show login or not
+            App.setPageTitle("Religious Request (Aidan Mulcahey)"); //set AppBar title
+            App.setHelpText(""); //set help text
+            App.setStackPane(stackPane); // required for dialog boxes, otherwise set null?
+            appBarComponent = FXMLLoader.load(getClass().getResource("/edu/wpi/cs3733/D21/teamE/fxml/AppBarComponent.fxml"));
+            appBarAnchorPane.getChildren().add(appBarComponent); //add FXML to this page's anchorPane element
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         locations = DB.getAllNodeLongNames();
         nodeID = DB.getListOfNodeIDS();
         //TODO add user type

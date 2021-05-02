ec216c485bb71a7c728567ce023928ab98c024e1
shanedonahue <skdonahue07@gmail.com>
2021-05-02 11:40:38 -0400

    appointment skeleton

---
+++ src/main/java/edu/wpi/cs3733/D21/teamE/scheduler/Appointment.java
@@ -0,0 +1,16 @@
+package edu.wpi.cs3733.D21.teamE.scheduler;
+
+import edu.wpi.cs3733.D21.teamE.Time;
+import edu.wpi.cs3733.D21.teamE.views.serviceRequestObjects.ServiceRequestObjs;
+
+
+public class Appointment {
+    private ServiceRequestObjs service;
+    private String title;
+
+    private Time startTime;
+    private Time duration;
+
+
+    public Appointment(){}
+}

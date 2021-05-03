23d94126065fb05b214cc645bd2362d67bfb95f1
RVRX <colemanning@yandex.com>
2021-05-03 12:58:54 -0400

    Add password validator (buggy)

--- src/main/java/edu/wpi/cs3733/D21/teamE/views/createAccount.java
+++ src/main/java/edu/wpi/cs3733/D21/teamE/views/createAccount.java
@@ -71,21 +71,27 @@ public class createAccount {
 			e.printStackTrace();
 		}
 
-		//init validators
+		//email validator
 		RegexValidator emailValidator = new RegexValidator();
 		emailValidator.setRegexPattern("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])");
 		emailValidator.setMessage("Invalid Email");
 
-//		RegexValidator passwordValidator = new RegexValidator();
-//		passwordValidator.setRegexPattern();
-		/*todo add FontAwesome Icon
-			FontIcon warnIcon = new FontIcon(FontAwesomeSolid.EXCLAMATION_TRIANGLE);
-			warnIcon.getStyleClass().add(ERROR);
-			validator.setIcon(warnIcon);*/
 		email.getValidators().add(emailValidator);
 		email.focusedProperty().addListener((o, oldVal, newVal) -> {
 			if (!newVal) {
 				email.validate();
+			}
+		});
+
+		//password validator
+		RegexValidator passwordValidator = new RegexValidator();
+		passwordValidator.setMessage("Invalid Password");
+		passwordValidator.setRegexPattern("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$");
+
+		password.getValidators().add(passwordValidator);
+		password.focusedProperty().addListener((o, oldVal, newVal) -> {
+			if (!newVal) {
+				password.validate();
 			}
 		});
 	}

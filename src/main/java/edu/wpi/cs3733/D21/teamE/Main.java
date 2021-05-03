6ce626ae69903c45ac97850906fb1a492108e8c2
Cole Manning // RVRX <colemanning@yandex.com>
2021-05-02 23:32:39 -0400

    Maybe working log stuff

--- src/main/java/edu/wpi/cs3733/D21/teamE/Main.java
+++ src/main/java/edu/wpi/cs3733/D21/teamE/Main.java
@@ -1,8 +1,33 @@
 package edu.wpi.cs3733.D21.teamE;
 
+
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
 public class Main {
+	//define logger
+	static Logger logger = Logger.getLogger(Main.class.getName());
+
 
 	public static void main(String[] args) {
+
+		try {
+			FileHandler handler = new FileHandler("BWHApplication.log", true);
+			logger.addHandler(handler);
+			SimpleFormatter formatter = new SimpleFormatter();
+			handler.setFormatter(formatter);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+
+		logger.setLevel(Level.ALL);
+		logger.info("Starting Application");
+
+
+
 		App.launch(App.class, args);
 	}
 }

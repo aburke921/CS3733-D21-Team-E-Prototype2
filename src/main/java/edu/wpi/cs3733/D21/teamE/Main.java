4eaf736f6058ff329cd1aaa53477c6d76c929069
Cole Manning // RVRX <colemanning@yandex.com>
2021-05-03 00:14:45 -0400

    Add some test logs #265

--- src/main/java/edu/wpi/cs3733/D21/teamE/Main.java
+++ src/main/java/edu/wpi/cs3733/D21/teamE/Main.java
@@ -7,27 +7,30 @@ import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
 public class Main {
+
 	//define logger
-	static Logger logger = Logger.getLogger(Main.class.getName());
+	static Logger logger = Logger.getLogger("BWH");
 
 
 	public static void main(String[] args) {
 
 		try {
+			//define handler
 			FileHandler handler = new FileHandler("BWHApplication.log", true);
-			logger.addHandler(handler);
 			SimpleFormatter formatter = new SimpleFormatter();
 			handler.setFormatter(formatter);
+			handler.setLevel(Level.ALL);
+
+			//add handler to logger
+			logger.addHandler(handler);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
-
 		logger.setLevel(Level.ALL);
+
+
 		logger.info("Starting Application");
-
-
-
 		App.launch(App.class, args);
 	}
 }

6e139a8916d8d45dad139cf36758313d9c89e714
shanedonahue <skdonahue07@gmail.com>
2021-05-04 12:30:04 -0400

    made class abstract

--- src/main/java/edu/wpi/cs3733/D21/teamE/pathfinding/algorithms/Searcher.java
+++ src/main/java/edu/wpi/cs3733/D21/teamE/pathfinding/algorithms/Searcher.java
@@ -13,7 +13,7 @@ import edu.wpi.cs3733.D21.teamE.database.makeConnection;
  * Abstract Searcher Class for Pathfinding API
  * On creation can be initialized to A* or DFS (already implemented) or others that can be added later
  */
-public class Searcher {
+public abstract class Searcher {
 
     private SearchConstraint type;
 
@@ -62,72 +62,10 @@ public class Searcher {
         return graph.get(nodeId);
     }
 
-    /**
-     * @param nodeId The Id of the node you want to get the neighbors of
-     * @return the neighbors of that node
-     */
-    public List<String> getNeighbors(String nodeId){
-        return graph.get(nodeId).getNeighbors();
-    }
-
     public Path search(String startId, String endId){
         Node start = getNode(startId);
         Node end = getNode(endId);
         return search(start, end);
-    }
-
-    /**
-     * Generic Search method for UI
-     * Searches between Start and End Nodes for path
-     * Searching algorithm can be A* or DFS (more can be added in the future)
-     *
-     * @param start The NodeID of the start node as a string
-     * @param end   The NodeID of the end node as a string
-     * @return The path from Start to End as a Path (basically a LinkedList)
-     */
-    public Path search(Node start, Node end){
-
-        PriorityQueue<Node> potentials = new PriorityQueue<>();
-        HashMap<Node, Double> prevCost = new HashMap<>();
-        HashMap<Node, Node> cameFrom = new HashMap<>();
-
-        Double zero = Double.valueOf(0);
-        prevCost.put(start, zero);
-        start.setCost(zero);
-
-        potentials.add(start);
-
-        while(!potentials.isEmpty()){
-            Node current = potentials.poll();
-            if(current.equals(end)){
-                //success case
-                Path path = new Path();
-                path.add(start);
-                path.add(reconstructPath(cameFrom, current));
-                return path;
-            } else if(isExcluded(current)){
-                continue;
-            }
-
-            List<String> neighbors = getNeighbors(current.get("id"));
-
-            for(String neighborId : neighbors){
-                Node neighbor = getNode(neighborId);
-                Double neighborCost = prevCost.get(current) + current.dist(neighbor);
-                if(!prevCost.containsKey(neighbor) || neighborCost < prevCost.get(neighbor)){
-                    prevCost.put(neighbor, neighborCost);
-                    cameFrom.put(neighbor, current);
-                    neighbor.setCost(neighborCost + neighbor.dist(end));
-
-                    //remove and re insert because value has been updated
-                    potentials.remove(neighbor);
-                    potentials.add(neighbor);
-                }
-            }
-        }
-
-        //failure case
-        return null;
     }
 
     public Path search(Object start, Object end){
@@ -160,6 +98,17 @@ public class Searcher {
             return null;
         }
     }
+
+    /**
+     * Generic Search method for UI
+     * Searches between Start and End Nodes for path
+     * Searching algorithm can be A* or DFS (more can be added in the future)
+     *
+     * @param start The NodeID of the start node as a string
+     * @param end   The NodeID of the end node as a string
+     * @return The path from Start to End as a Path (basically a LinkedList)
+     */
+    public abstract Path search(Node start, Node end);
 
     public Path searchAlongPath(Path route, String stopType){
         List<Node> stops = DB.getAllNodesByType(stopType);

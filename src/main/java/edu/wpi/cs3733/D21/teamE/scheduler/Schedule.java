4598129db077d8ca9af4c8c147e64f6cf6d4e7bf
shanedonahue <skdonahue07@gmail.com>
2021-05-07 16:40:58 -0400

    added schedule functionality

--- src/main/java/edu/wpi/cs3733/D21/teamE/scheduler/Schedule.java
+++ src/main/java/edu/wpi/cs3733/D21/teamE/scheduler/Schedule.java
@@ -1,12 +1,56 @@
 package edu.wpi.cs3733.D21.teamE.scheduler;
 
-import java.util.LinkedList;
-import java.util.List;
+import edu.wpi.cs3733.D21.teamE.map.Node;
+import java.util.*;
 
 public class Schedule {
-    private List<Todo> schedule;
+    private final List<Todo> schedule;
 
-    public Schedule(){
-        this.schedule = new LinkedList<>();
+    public Schedule(List<Todo> todoList){
+        schedule = new ArrayList<Todo>(todoList.size()){
+            @Override
+            public boolean add(Todo o) {
+                if(isAvailable(o) && super.add(o)){
+                    Collections.sort(schedule);
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        };
+        for(Todo todo : todoList){
+            //noinspection UseBulkOperation
+            schedule.add(todo);
+        }
     }
+
+    public boolean add(Todo newTodo){
+        return schedule.add(newTodo);
+    }
+
+    public boolean isAvailable(Todo t) {
+        boolean isAvailable = true;
+        Iterator<Todo> itr = schedule.iterator();
+        while(isAvailable && itr.hasNext()){
+            isAvailable &= !(t.isConflict(itr.next()));
+        }
+        return isAvailable;
+    }
+
+    public List<Node> getLocations(){
+        List<Node> locations = new LinkedList<>();
+        for(Todo event : schedule){
+            locations.add(event.getLocation());
+        }
+        return locations;
+    }
+
+    public List<Todo> getSchedule(){
+        return schedule;
+    }
+
+    public Todo get(int index){
+        return schedule.get(index);
+    }
+
 }

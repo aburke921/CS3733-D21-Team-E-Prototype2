21a9993950e21036262211248ff35772237bc8c5
geekblack22 <geekblack22@users.noreply.github.com>
2021-05-11 17:27:10 -0400

    added credit and about page

---
+++ src/main/java/edu/wpi/cs3733/D21/teamE/views/About.java
@@ -0,0 +1,23 @@
+package edu.wpi.cs3733.D21.teamE.views;
+
+import edu.wpi.cs3733.D21.teamE.App;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+
+public class About {
+
+    @FXML
+    private void exit(ActionEvent actionEvent){
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/edu/wpi/cs3733/D21/teamE/fxml/Default.fxml"));
+            App.getPrimaryStage().getScene().setRoot(root);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+}

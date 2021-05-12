fef11bcce549352f0f49cc79dace3e8bb9b6b826
Shannen <sklin@wpi.edu>
2021-05-11 20:36:39 -0400

    Fixed fxml pages for credit page and about us page

--- src/main/java/edu/wpi/cs3733/D21/teamE/views/About.java
+++ src/main/java/edu/wpi/cs3733/D21/teamE/views/About.java
@@ -2,21 +2,42 @@ package edu.wpi.cs3733.D21.teamE.views;
 
 import com.jfoenix.controls.JFXButton;
 import edu.wpi.cs3733.D21.teamE.App;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 
-import java.awt.event.ActionEvent;
 import java.io.IOException;
 
 public class About {
 
-    @FXML // fx:id="exit"
-    private JFXButton exit;
+    @FXML // fx:id="imageView"
+    private ImageView imageView;
+
+    @FXML
+    private AnchorPane appBarAnchorPane;
 
     @FXML
     public void initialize() {
 
+        //init appBar
+        javafx.scene.Node appBarComponent = null;
+        try {
+            App.setShowHelp(false);
+            App.setShowLogin(true);
+            App.setPageTitle("About Us");
+            App.setHelpText(""); //todo help text
+            appBarComponent = FXMLLoader.load(getClass().getResource("/edu/wpi/cs3733/D21/teamE/fxml/AppBarComponent.fxml"));
+            appBarAnchorPane.getChildren().add(appBarComponent); //add FXML to this page's anchorPane element
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Image about = new Image("edu/wpi/cs3733/D21/teamE/about.png");
+        imageView.setImage(about);
     }
 
     @FXML
@@ -29,4 +50,5 @@ public class About {
         }
 
     }
+
 }
--- src/main/java/edu/wpi/cs3733/D21/teamE/views/Credit.java
+++ src/main/java/edu/wpi/cs3733/D21/teamE/views/Credit.java
@@ -2,20 +2,42 @@ package edu.wpi.cs3733.D21.teamE.views;
 
 import com.jfoenix.controls.JFXButton;
 import edu.wpi.cs3733.D21.teamE.App;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 
-import java.awt.event.ActionEvent;
 import java.io.IOException;
 
 public class Credit {
 
-    @FXML // fx:id="exit"
-    private JFXButton exit;
+    @FXML // fx:id="imageView"
+    private ImageView imageView;
+
+    @FXML
+    private AnchorPane appBarAnchorPane;
 
     @FXML
     public void initialize() {
+
+        //init appBar
+        javafx.scene.Node appBarComponent = null;
+        try {
+            App.setShowHelp(false);
+            App.setShowLogin(true);
+            App.setPageTitle("Credit Page");
+            App.setHelpText(""); //todo help text
+            appBarComponent = FXMLLoader.load(getClass().getResource("/edu/wpi/cs3733/D21/teamE/fxml/AppBarComponent.fxml"));
+            appBarAnchorPane.getChildren().add(appBarComponent); //add FXML to this page's anchorPane element
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Image credit = new Image("edu/wpi/cs3733/D21/teamE/credit.png");
+        imageView.setImage(credit);
 
     }
 
